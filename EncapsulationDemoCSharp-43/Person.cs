92e22020731210a79e8ab0ce6d64ea59a4c1aac0
Seth Bowman <brandonsethbowman@gmail.com>
2023-12-19 10:32:07 -0600

    Add project files.

---
+++ EncapsulationDemoCSharp-43/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDemoCSharp_43
+{
+    public class Person
+    {
+        //properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //fields
+        private double _wallet;
+
+        //methods that access our private field
+        public void AddMoney(double moneyToAdd)
+        {
+            _wallet += moneyToAdd;
+        }
+
+        public double CheckWallet()
+        {
+            return _wallet;
+        }
+    }
+}
---
+++ EncapsulationDemoCSharp-43/Program.cs
@@ -0,0 +1,19 @@
+﻿using EncapsulationDemoCSharp_43;
+
+var seth = new Person();
+
+seth.Name = "Seth";
+seth.Age = 29;
+
+seth.AddMoney(20.00);
+
+var currentMoney = seth.CheckWallet();
+
+Console.WriteLine(currentMoney);
+
+seth.AddMoney(500.00);
+
+currentMoney = seth.CheckWallet();
+
+Console.WriteLine(currentMoney);
+
